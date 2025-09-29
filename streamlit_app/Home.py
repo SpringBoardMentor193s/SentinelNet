044e1bc4dfca6ab3906b5197aaa14dfb99361701
bhavanathota2006-code <bhavanathota2006@gmail.com>
2025-09-29 12:34:02 +0530

    Added  streamlit_app

---
+++ streamlit_app/Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="SentinelNet", layout="centered")
+
+st.title("🔐 SentinelNet IDS - Model Selection")
+
+# Dropdown for models
+model_choice = st.selectbox(
+    "Select a Model:",
+    ["Logistic Regression", "Decision Tree", "Random Forest"]
+)
+
+# Save selection in session_state
+if st.button("Next ➡️"):
+    st.session_state["selected_model"] = model_choice
+    st.switch_page("pages/Feature_Input.py")
